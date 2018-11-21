74632ad9b0b0d926273158b986930df739262dee
caroline <caroline@student-218-200.eduroam.uu.se>
2018-11-21 12:58:35 +0100

    update

--- Lab1/levmarq.m
+++ Lab1/levmarq.m
@@ -4,10 +4,12 @@ function [x,resnorm,residual] = levmarq(func,x0)
     stop = 0;
     lambda = 0.01;
     [Fx,GrFx] = feval(func, x);
+    
     fx =double(Fx'*Fx);
     I = eye(lx);
     steps = 0;
     resnorm = [];
+    residual =[];
     while stop == 0   
         steps = steps+1;
         first = GrFx*Fx;
@@ -24,10 +26,10 @@ function [x,resnorm,residual] = levmarq(func,x0)
            lambda = lambda /10;
            x =next; 
            resnorm =[resnorm;norm(est_Fx)];
-           residual =est_Fx;
            Fx=est_Fx;
            GrFx=est_GrFx;
-           fx = estimate_fx
+           fx = estimate_fx;
+           residual =[residual;fx];
         else
             lambda = lambda * 10;
         end
@@ -37,5 +39,19 @@ function [x,resnorm,residual] = levmarq(func,x0)
         end
     end
     plot(resnorm);
+end
+
+function J = findjac(FUN,r,x)
+epsx = 1e-4;
+lx=length(x);
+J=zeros(length(r),lx);
+for k=1:lx
+    dx=.25*epsx(k);
+    xd=x;
+    xd(k)=xd(k)+dx;
+    rd=feval(FUN,xd);
+%   ~~~~~~~~~~~~~~~~    
+    J(:,k)=((rd-r)/dx);
+end
 end
 
