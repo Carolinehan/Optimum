f80b9fa728a4789730f6105f34fa7ab618a1d195
Caroline <13162753082@163.com>
2018-12-02 16:46:25 +0100

    lab2

--- Lab2/assignment2.m
+++ Lab2/assignment2.m
@@ -3,13 +3,16 @@ data=load('wdbc2.dat');
 f_count=30;
 x=data(:,3:32);
 y=data(:,2);
-ma = {'ko','ks'};
-fc = {[0 0 0],[1 1 1]};
-tv = unique(y)
-figure(1); hold off
-for i = 1:length(tv)
-    pos = find(y==tv(i));
-    plot(x,ma{i},'markerfacecolor',fc{i});
-    hold on
-end
-
+n = size(x,1); %row numbers
+m = size(x,2); % column numbers
+c=1000;
+H = diag([ones(1, m), zeros(1, n + 1)]);
+f = [zeros(1,m+1) c*ones(1,n)]';
+p = diag(y) * x;
+A = -[p y eye(n)];
+B = -ones(n,1);
+lb = [-inf * ones(m+1,1) ;zeros(n,1)];
+z = quadprog(H,f,A,B,[],[],lb);
+w = z(1:m,:)
+b = z(m+1:m+1,:)
+eps = z(m+2:m+n+1,:)
